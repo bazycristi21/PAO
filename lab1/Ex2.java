448b77c776b69a39fafeb0f8ce186b8bff660bd0
Bazavan Cristian - Valeriu <bazy_cristi_21@yahoo.com>
2020-02-27 19:35:50 +0200

    Ex2

--- lab1/Ex2.java
+++ lab1/Ex2.java
@@ -1,1 +1,27 @@
-1
+import java.util.Scanner;
+public class Exemplul2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Data:");
+        System.out.print("Input first integer: ");
+        int a = input.nextInt();
+        
+        System.out.print("Input second integer: ");
+        int b = input.nextInt();
+        
+        System.out.println("Expected output: ");
+        if(a < b)
+            System.out.println(a + " < " + b);
+        if(a > b)
+            System.out.println(a + " > " + b);
+        if(a == b) 
+            System.out.println(a + " == " + b);
+        if(a != b)
+            System.out.println(a + " != " + b);
+        if(a <= b)
+            System.out.println(a + " <= " + b);
+        if(a >= b)
+            System.out.println(a + " < " + b);      
+        
+    }
+}
