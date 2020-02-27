b45c154ff4173d0d050d02ff6e35bed6923db952
Bazavan Cristian - Valeriu <bazy_cristi_21@yahoo.com>
2020-02-27 19:34:38 +0200

    Ex5

--- lab1/Ex5.java
+++ lab1/Ex5.java
@@ -1,1 +1,22 @@
-a
+import java.util.Scanner;
+public class P6 {
+    public static void main(String[] argv) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int fib1= 1, fib2= 1;
+        int aux = 2;
+        if (x == 1 || x == 2) {
+            System.out.println("1");
+        }
+        else {
+            x = x - 2;
+            while (x > 0) {
+                aux = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = aux;
+                x--;
+            }
+            System.out.println(aux);
+        }
+    }
+}
