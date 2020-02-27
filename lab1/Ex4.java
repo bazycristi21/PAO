37fb8c9c5f45d38a037bad43cfd91c7cf88761bf
Bazavan Cristian - Valeriu <bazy_cristi_21@yahoo.com>
2020-02-27 19:34:11 +0200

    Ex4

--- lab1/Ex4.java
+++ lab1/Ex4.java
@@ -1,1 +1,16 @@
-E
+import java.util.Scanner;
+public class problema5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        if (x == 0) {
+            System.out.print("0");
+        } else {
+            int fac = 1, i = 2;
+            for (i = 2; i <= x; i++) {
+                fac = fac * i;
+            }
+            System.out.print(fac);
+        } 
+    }
+}
